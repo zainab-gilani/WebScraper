373aedcea8121e3d0e5a1d102822786d365fa897
zainab-gilani <zainab.gilani@yahoo.com>
2025-05-28 08:26:35 +0400

    Selected HTML elements with BeautifulSoup

--- scraper.py
+++ scraper.py
@@ -12,3 +12,26 @@ headers: set[str] = {
 page: Response = requests.get('https://www.ucas.com/explore/search/providers?query=', headers=headers)
 
 soup = BeautifulSoup(page.text, "html.parser")
+
+# get all <h1> elements
+# on the page
+h1_elements = soup.find_all('h1')
+
+# get the element with id="main-title"
+main_title_element = soup.find(id='main-title')
+
+# find the footer element
+# based on the text it contains
+footer_element = soup.find(text={'Powered by WordPress'})
+
+# find all the centered elements
+# on the page
+centered_element = soup.find_all(class_='text-center')
+
+# get all "li" elements
+# in the ".navbar" element
+soup.find(class_='navbar').find_all('li')
+
+# get all "li" elements
+# in the ".navbar" element
+soup.select('.navbar > li')
