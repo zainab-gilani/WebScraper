053086f5de280f1fc907b24fe735dfd7e01f8538
zainab-gilani <zainab.gilani@yahoo.com>
2025-05-28 08:24:27 +0400

    Connected to target URL

---
+++ scraper.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+from requests import Response
+
+# Scraping logic...
+
+headers: set[str] = {
+    "User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+}
+
+# Missing closing parenthesis here:
+page: Response = requests.get('https://www.ucas.com/explore/search/providers?query=', headers=headers)
